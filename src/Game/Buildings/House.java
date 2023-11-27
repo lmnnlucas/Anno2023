e041507852e69876c05c052905e982bf18591977
Lucas <65688348+kurashyna@users.noreply.github.com>
2023-11-27 17:21:03 +0100

    ANO-3 House.java (#8)
    Co-authored-by: Lucas Le Menn <lucas.le-menn@etu.u-bordeaux.fr>

---
+++ src/Game/Buildings/House.java
@@ -0,0 +1,19 @@
+package Game.Buildings;
+
+import Game.Resource;
+
+import java.util.HashMap;
+
+public class House extends Building {
+    public House() {
+        super("House", 0, 4, 4, 1, 1);
+
+        this.setResourcesNeeded(new HashMap<>() {{
+            put(Resource.WOOD, 2);
+            put(Resource.STONE, 2);
+        }});
+
+        this.setResourcesGenerating(null);
+        this.setResourcesConsumption(null);
+    }
+}
