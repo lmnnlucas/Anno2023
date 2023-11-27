2ec033a08c2ed3fb45d031ab4b5b4bc38d6b70f5
Lucas <65688348+kurashyna@users.noreply.github.com>
2023-11-27 17:12:02 +0100

    ANO-7 LumberMill.java

---
+++ src/Game/Buildings/LumberMill.java
@@ -0,0 +1,24 @@
+package Game.Buildings;
+
+import Game.Resource;
+
+import java.util.HashMap;
+
+public class LumberMill extends Building {
+    public LumberMill() {
+        super("Lumber Mill", 10, 0, 4, 1, 6);
+
+        this.setResourcesNeeded(new HashMap<>() {{
+            put(Resource.WOOD, 50);
+            put(Resource.STONE, 50);
+        }});
+
+        this.setResourcesConsumption(new HashMap<>() {{
+            put(Resource.WOOD, 4);
+        }});
+
+        this.setResourcesGenerating(new HashMap<>() {{
+            put(Resource.LUMBER, 4);
+        }});
+    }
+}
