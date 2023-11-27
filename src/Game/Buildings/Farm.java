c18a751ff49a85e2d39f139d4a1214137fc6998d
ireyzzen <102664717+ireyzzen@users.noreply.github.com>
2023-11-27 16:50:03 +0100

    ANO-5 : fin de la création de la classe Farm (#1)

---
+++ src/Game/Buildings/Farm.java
@@ -0,0 +1,23 @@
+package Game.Buildings;
+
+import java.util.HashMap;
+
+import Game.Resource;
+
+public class Farm extends Building {
+
+    public Farm() {
+        super("Farm", 3, 5, 2, 1, 4);
+        System.out.println("Build Farm");
+        this.setResourcesConsumption(null); // TODO : Remplacer par des 0.
+        this.setResourcesNeeded(new HashMap<>(){{
+            put(Resource.WOOD, 5);
+            put(Resource.STONE,5);
+        }});
+
+        this.setResourcesGenerating( new HashMap<>(){{
+            put(Resource.FOOD, 10);
+        }});
+    }
+
+}
