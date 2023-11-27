c52b7fc8aff80c3a87b4028425ef6758ca5f11ca
Quentin Marrot <quentin.marrot@etu.u-bordeaux.fr>
2023-11-27 17:07:11 +0100

    ANO-8 : CementPlant Fait

---
+++ src/Game/Buildings/CementPlant.java
@@ -0,0 +1,27 @@
+package Game.Buildings;
+
+import Game.Resource;
+
+import java.util.HashMap;
+
+public class CementPlant extends Building
+{
+    private final String name = "Cement Plant";
+    public CementPlant() {
+        super("Cement Plant", 10, 0, 2, 1, 4);
+        this.setResourcesConsumption(new HashMap<>()
+         {{
+             put(Resource.COAL, 4);
+             put(Resource.STONE, 4);
+         }}
+        );
+        this.setResourcesNeeded(new HashMap<>(){{
+            put(Resource.WOOD, 50);
+            put(Resource.STONE, 50);
+        }});
+
+        this.setResourcesGenerating( new HashMap<>(){{
+            put(Resource.CEMENT, 4);
+        }});
+    }
+}
