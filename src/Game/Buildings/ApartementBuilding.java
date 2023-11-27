03fda3c62fdee00aa759fbb0fbc3a1842d26b5e8
Quentin Marrot <quentin.marrot@etu.u-bordeaux.fr>
2023-11-27 16:53:58 +0100

    ANO-4 ApartementBuilding fait

---
+++ src/Game/Buildings/ApartementBuilding.java
@@ -0,0 +1,20 @@
+package Game.Buildings;
+
+import Game.Resource;
+
+import java.util.HashMap;
+
+public class ApartementBuilding extends Building
+{
+    private final String name = "Apartement Building";
+    public ApartementBuilding() {
+        super("Apartement Building", 0, 60, 6, 1, 4);
+        this.setResourcesConsumption(null);
+        this.setResourcesNeeded(new HashMap<>(){{
+            put(Resource.WOOD, 50);
+            put(Resource.STONE, 50);
+        }});
+
+        this.setResourcesGenerating(null);
+    }
+}
