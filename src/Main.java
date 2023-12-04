3383d2ead4e7cf3aa066b5ed717fe45cdc625b0a
Quentin Marrot <quentin.marrot@etu.u-bordeaux.fr>
2023-12-04 16:45:28 +0100

    ANO-25-PRINTGAME fin du print game I guess

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 
+import Game.Buildings.CementPlant;
+import Game.Buildings.House;
 import Manager.*;
 import UI.*;
+import Game.*;
 public class Main {
     public static void main(String[] args)
         {
             Manager manager = new Manager(10 , 10);
             UI ui = new UI(manager);
+            manager.setBuilding(new Position(1,1),new CementPlant());
             ui.printWorld();
+            ui.printResource();
             ui.waitEntry();
+            ui.printWorld();
         }
     }
