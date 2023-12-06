493e12de0311463575c0d69057311a543fb1a753
Maxime OULEDI <maxime.ouledi@etu.u-bordeaux.fr>
2023-12-06 13:37:35 +0100

    Ajout des commentaires dans le fichier Manager

--- src/Manager/Manager.java
+++ src/Manager/Manager.java
@@ -12,12 +12,36 @@ import Game.Buildings.House;
 import Game.People.Citizen;
 import UI.UI;
 
+/**
+ * public class Manager
+ * Controler class
+ * Define structure of the data and the game
+ * Update observers and comunicate with UI
+ */
 public class Manager{
+    /**
+     * width of the game
+     */
     private final int width;
+    /**
+     * height of the game
+     */
     private final int height;
+    /**
+     * round of the game
+     */
     private int round = 0;
+    /**
+     * UI of the game
+     */
     private UI ui;
+    /**
+     * List of observers
+     */
     private ArrayList<Observer> observers = new ArrayList<>();
+    /**
+     * List of resources of the player
+     */
     private HashMap<Resource,Integer> resources = new HashMap<>(){{ //Config initial
         put(Resource.GOLD, 10);
         put(Resource.FOOD, 100);
@@ -30,9 +54,20 @@ public class Manager{
         put(Resource.LUMBER, 0);
         put(Resource.TOOLS, 0);
     }};
+    /**
+     * List of buildings on the game
+     */
     private HashMap<Position,Building> building = new HashMap<>();
+    /**
+     * List of citizens alive in the game
+     */
     private ArrayList<Citizen> citizens = new ArrayList<>();
 
+    /**
+     * Constructor of the class
+     * @param width width of the game
+     * @param height height of the game
+     */
     public Manager(int width,int height){
         this.width = width;
         this.height = height;
@@ -42,7 +77,10 @@ public class Manager{
         initializeObserver();
 
     }
-
+    /**
+     * Constructor of the class
+     * Default width and height
+     */
     public Manager(){
         this.width = 10;
         this.height = 10;
@@ -52,42 +90,97 @@ public class Manager{
         initializeObserver();
 
     }
-    public void initializeObserver(){
+    /**
+     * Initialize the list of observers
+     */
+    private void initializeObserver(){
         observers.add(new BuildingsConsumingObserver(this));
         observers.add(new CitizenConsumingObserver(this));
     }
-
+    /**
+     * Getter of the width
+     * @return width of the game
+     */
     public int getWidth(){
         return width;
     }
+    /**
+     * Getter of the height
+     * @return height of the game
+     */
     public int getHeight(){
         return height;
     }
-
+    /**
+     * Getter of the round
+     * @return round of the game
+     */
     public int getRound(){
         return round;
     }
 
-    public void setResource(Resource r, int i){
+    /**
+     * Getter of the number of a ressource
+     * @param r ressource to get
+     * @return
+     */
+    public int getNumberRessource(Resource r){
+        return resources.get(r);
+    }
+
+    /**
+     * Getter of the list of resources
+     * @return list of resources
+     */
+
+    public HashMap<Resource,Integer> getResources(){
+        return resources;
+    }
+    
+    /**
+     * Getter of the list of citizens
+     * @return list of citizens
+     */
+    public List<Citizen> getCitizens(){
+        return citizens;
+    }
+
+    /**
+     * Getter of the list of buildings
+     * @return list of buildings
+     */
+
+    public HashMap<Position,Building> getBuildings(){
+        return building;
+    }
+
+    /**
+     * Setter of the resources
+     * @param r ressource to add
+     * @param i number of ressource to add
+     */
+
+     public void setResource(Resource r, int i){
         resources.put(r, resources.get(r)+i);
     }
-    public void citizenDeath(Citizen c){
+    
+    /**
+     * Remove a citizen to the game
+     * @param c citizen to remove
+     */
+    
+     public void citizenDeath(Citizen c){
         c.getHome().removeCitizen(c);
         c.getWorkplace().removeWorker(c);
         citizens.remove(c);
     }
-    public int getNumberRessource(Resource r){
-        return resources.get(r);
-    }
-    public HashMap<Resource,Integer> getResources(){
-        return resources;
-    }
-    public List<Citizen> getCitizens(){
-        return citizens;
-    }
-    public HashMap<Position,Building> getBuildings(){
-        return building;
-    }
+
+    /**
+     * Setter of the list of buildings
+     * @param pos position of the building
+     * @param b building to add
+     */
+
     public void setBuilding(Position pos,Building b){
         int cpt = 0;
         int length = b.getResourcesNeeded().size();
@@ -106,9 +199,13 @@ public class Manager{
             }
             addRound();
         }
-       
-     
     }
+
+    /**
+     * Remove a building to the game
+     * @param pos position of the building
+     */
+
     public void removeBuilding(Position pos){
         building.remove(pos);
         resources.put(Resource.GOLD,resources.get(Resource.GOLD) + building.get(pos).getResourcesNeeded().get(Resource.GOLD)/2);
@@ -124,26 +221,54 @@ public class Manager{
         resources.put(Resource.TOOLS,resources.get(Resource.TOOLS) + building.get(pos).getResourcesNeeded().get(Resource.TOOLS)/2);
          */
     }
+
+    /**
+     * Add a round to the game
+     */
+
     public void addRound(){
         round++;
     }
+
+    /** 
+     * Notify all the observers 
+    */
 
     public void notifyObserver(){
         for(Observer o : observers){
             o.update();
         }
     }
-    public void printWorld(){
+    
+    /**
+     * Ask the UI to print the world
+     */
+    
+     public void printWorld(){
         ui.printWorld();
     }
+
+    /**
+     * Ask the UI to wait the entry of the player
+     */
+
     public void waitEntry(){
         ui.waitEntry();
     }
+
+    /**
+     * Ask the UI to print the resources
+     */
 
     public void printResource(){
         ui.printResource();
     }
 
+    /**
+     * Add workers to a building
+     * @param c citizen to add
+     * @param b building where the citizen is added
+     */
     public void AddWorkerToABuilding(Building b,int number){
         int cpt = 0;
         for (int i = 0; i< citizens.size();i++){
@@ -160,7 +285,11 @@ public class Manager{
         }
         addRound();
     }
-
+    /**
+     * Remove workers to a building
+     * @param b building where the citizen is removed
+     * @param number number of citizen removed
+     */
     public void removeWorkerFromBuilding(Building b,int number){
         int cpt = 0;
         for (int i = 0; i< citizens.size();i++){
@@ -178,6 +307,11 @@ public class Manager{
         addRound();
     }
 
+    /**
+     * Add citizen to a building 
+     * @param b building where the citizen is add
+     * @param number number of citizen added
+     */
     public void addCitizenToABuilding(Building b,int number){
         int cpt = 0;
         for (int i = 0; i< citizens.size();i++){
@@ -194,7 +328,11 @@ public class Manager{
         }
         addRound();
     }
-
+    /**
+     * Remove citizen to a building 
+     * @param b building where the citizen is removed
+     * @param number number of citizen removed
+     */
     public void removeCitizenFromBuilding(Building b,int number){
         int cpt = 0;
         for (int i = 0; i< citizens.size();i++){
@@ -211,7 +349,10 @@ public class Manager{
         }
         addRound();
     }
-
+    /**
+     * Get the number of citizen without home
+     * @return number of homeless
+     */
     public int getNumberHomeless(){
         int cpt = 0;
         for (int i = 0; i < citizens.size();i++){
@@ -221,6 +362,10 @@ public class Manager{
         }
         return cpt;
     }
+    /**
+     * Get the number of workers without work
+     * @return number of workless 
+     */
     public int getNumberWorkless(){
         int cpt = 0;
         for (int i =0; i < citizens.size();i++){
@@ -230,6 +375,11 @@ public class Manager{
         }
         return cpt;
     }
+
+    /**
+     * Check if the game is finished
+     * @return true if the game is finished
+     */
 
     public boolean isFinished(){
         if (citizens.size() == 0){
